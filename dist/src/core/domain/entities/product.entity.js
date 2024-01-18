c64db6b3cd6fa77f3726fd594aba7f5a0fa3be05
Jefferson Estevam <jesbmail@gmail.com>
2024-01-18 09:49:27 -0300

    delete: removed dist folder

--- dist/src/core/domain/entities/product.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Product {
-    sku;
-    name;
-    isMarketable;
-    inventory;
-}
-exports.default = Product;
--- dist/src/core/domain/entities/warehouse.entity.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Warehouse {
-    locality;
-    quantity;
-    type;
-}
-exports.default = Warehouse;
